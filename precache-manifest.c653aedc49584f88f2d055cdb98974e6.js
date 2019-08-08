4c73fde6e54c76bfee08ac9dc0cc7e363fc5149a
enano4k <enano4k@gmail.com>
2019-08-08 00:03:44 -0600

    other test

--- precache-manifest.c653aedc49584f88f2d055cdb98974e6.js
+++ precache-manifest.c653aedc49584f88f2d055cdb98974e6.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/atomicbuster_rinkeby/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "98f8b2fcdeca041700d1",
-    "url": "/static/js/main.98f8b2fc.chunk.js"
+    "url": "/atomicbuster_rinkeby/static/js/main.98f8b2fc.chunk.js"
   },
   {
     "revision": "5709af443de51042cf60",
-    "url": "/static/js/1.5709af44.chunk.js"
+    "url": "/atomicbuster_rinkeby/static/js/1.5709af44.chunk.js"
   },
   {
     "revision": "8c2edbdb726b28d8bf852bff92a9848c",
